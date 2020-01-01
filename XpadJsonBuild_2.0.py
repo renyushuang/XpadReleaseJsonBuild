363a9d03029f86df10f2b8168fae0b21296790b4
“renyushuang” <renyushuang501@163.com>
2020-01-01 12:26:32 +0800

    change: 增加UI界面

--- XpadJsonBuild_2.0.py
+++ XpadJsonBuild_2.0.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 import openpyxl, os, json, sys, logging
 from openpyxl.worksheet.worksheet import Worksheet
+from tkinter import *
+from tkinter import filedialog
 
 AD_TYPE_OPEN = "开屏"
 AD_TYPE_NATIVE_PLATE = "原生模版"
@@ -55,6 +57,16 @@ excelPath = ""
 
 mAdResultMap = {}
 
+filename = None
+path = None
+listBox: Listbox = None
+
+
+def insertListBoxMessage(item):
+    global listBox
+    listBox.insert("end", item)
+    listBox.see(END)
+
 
 def findTitleInColum(name, adSheet):
     maxColumn = int(adSheet.max_column)
@@ -62,9 +74,9 @@ def findTitleInColum(name, adSheet):
         titleValue = adSheet.cell(row=1, column=columnIndex + 1).value
         if titleValue == name:
             return columnIndex + 1
-
+    insertListBoxMessage("title 不存在 --" + name)
     logging.error("title 不存在 --" + name)
-    exit()
+    insertListBoxMessage("解析将会停止")
 
 
 def getTitleColumValue(name, adSheet):
@@ -72,7 +84,7 @@ def getTitleColumValue(name, adSheet):
     value = adSheet.cell(row=2, column=index).value
     if value is not None:
         return value
-
+    insertListBoxMessage("当前 -- " + name + "值为 None")
     logging.warning("当前 -- " + name + "值为 None")
 
     return ""
@@ -98,6 +110,7 @@ def hasAdTypeString(adDetailsValuelist: list, typeName):
 def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
     adDetailsValuelist: list = adDetailsValue.split("-")
     if len(adDetailsValuelist) < 2:
+        insertListBoxMessage("备注 命名错误" + str(adSourceIdValue))
         logging.error("备注 命名错误" + str(adSourceIdValue))
 
     if platformValue == AD_TYPE_CSJ:
@@ -114,6 +127,7 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         elif hasAdTypeString(adDetailsValuelist, AD_TYPE_BANNER):
             return AD_TYPE_CSJ_PERSONAL_PLATE_BANNER
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "穿山甲的广告id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "穿山甲的广告id为 = " + str(adSourceIdValue))
 
     elif platformValue == AD_TYPE_YLH:
@@ -128,6 +142,7 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         if hasAdTypeString(adDetailsValuelist, AD_TYPE_BANNER):
             return AD_TYPE_YLH_BANNER
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "广点通id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "广点通id为 = " + str(adSourceIdValue))
 
         pass
@@ -142,13 +157,16 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         if hasAdTypeString(adDetailsValuelist, AD_TYPE_NATIVE):
             return AD_TYPE_KSH_PERSONAL_PLATE_FEED
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "快手id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "快手id为 = " + str(adSourceIdValue))
 
         pass
     else:
+        insertListBoxMessage("没有这个类型 广告id是 = " + str(adSourceIdValue))
         logging.error("没有这个类型 广告id是 = " + str(adSourceIdValue))
-        exit()
+        insertListBoxMessage("解析将会停止")
 
+    insertListBoxMessage("不支持这种类型 " + adDetailsValue + "id为 = " + str(adSourceIdValue))
     logging.error("不支持这种类型 " + adDetailsValue + "id为 = " + str(adSourceIdValue))
     return None
 
@@ -190,11 +208,14 @@ def addChannelIds(slot: list, adSheet):
 
         platformValue = getCloumeValueColumValue(currentIndex, "Platform", adSheet)
         if platformValue is None:
+            insertListBoxMessage(
+                "Platform is None --- sid = " + str(channels["sid"]) + "----- 行 = " + str(currentIndex))
             logging.warning("Platform is None --- sid = " + str(channels["sid"]) + "----- 行 = " + str(currentIndex))
             continue
 
         adSourceIdValue = getCloumeValueColumValue(currentIndex, "广告ID", adSheet)
         if adSourceIdValue is None:
+            insertListBoxMessage("广告ID is None --- sid = " + str(channels["sid"]) + "----- 行 = " + str(currentIndex))
             logging.warning("广告ID is None --- sid = " + str(channels["sid"]) + "----- 行 = " + str(currentIndex))
             continue
 
@@ -202,16 +223,19 @@ def addChannelIds(slot: list, adSheet):
         if adWatingTimeValue is None:
             adWatingTimeValue = 4000
         elif adWatingTimeValue < 1000:
+            insertListBoxMessage("广告超时时间 是毫秒的 =" + str(adSourceIdValue) + "--对应 = " + adWatingTimeValue + " 是错误的")
             logging.error("广告超时时间 是毫秒的 =" + str(adSourceIdValue) + "--对应 = " + adWatingTimeValue + " 是错误的")
 
         adExpriedTImeValue = getCloumeValueColumValue(currentIndex, "广告过期时间", adSheet)
         if adExpriedTImeValue is None:
             adExpriedTImeValue = 40
         elif adExpriedTImeValue < 0:
+            insertListBoxMessage("广告过期时间 是分钟的 =" + str(adSourceIdValue) + "--对应 = " + adWatingTimeValue + " 是错误的")
             logging.error("广告过期时间 是分钟的 =" + str(adSourceIdValue) + "--对应 = " + adWatingTimeValue + " 是错误的")
 
         adDetailsValue = getCloumeValueColumValue(currentIndex, "备注", adSheet)
         if adDetailsValue is None:
+            insertListBoxMessage("备注内容为None  跳过= " + str(adSourceIdValue))
             logging.warning("备注内容为None  跳过= " + str(adSourceIdValue))
             continue
 
@@ -273,14 +297,15 @@ def addChannelIds(slot: list, adSheet):
     pass
 
 
-def main():
+def main(excelPath):
     # 开始读取
     wb = openpyxl.load_workbook(excelPath)
     sheetNames = wb.sheetnames
     adSheet: Worksheet = wb[str(sheetNames[0])]
 
     maxColumn = int(adSheet.max_column)
-
+    insertListBoxMessage("最大列 = " + str(maxColumn))
+    insertListBoxMessage("最大行 = " + str(adSheet.max_row))
     logging.info("最大列 = " + str(maxColumn))
     logging.info("最大行 = " + str(adSheet.max_row))
 
@@ -308,34 +333,39 @@ def main():
     mAdResultMap["status"] = 1
 
 
-if __name__ == '__main__':
+def startAndBuild(excelPath):
     logging.basicConfig(level=logging.INFO)
-
+    insertListBoxMessage("XPAD 2.0 json脚本生成工具")
     logging.info("XPAD 2.0 json脚本生成工具")
 
-    argLen = len(sys.argv)
-    if argLen < 2:
-        logging.error("请输入想要解析的文件")
-        exit()
-    excelPath = sys.argv[1]
+    # argLen = len(sys.argv)
+    # if argLen < 2:
+    #     logging.error("请输入想要解析的文件")
+    #     exit()
+    # excelPath = sys.argv[1]
 
     if not os.path.exists(excelPath):
+        insertListBoxMessage("需要解析的Excel文件 不存在")
         logging.error("需要解析的Excel文件 不存在")
-        exit()
+        insertListBoxMessage("解析将会停止")
 
     fileSub = os.path.splitext(os.path.basename(excelPath))[1]
     if fileSub != ".xlsx":
+        insertListBoxMessage("请输入正确的Excel文件->" + fileSub)
         logging.error("请输入正确的Excel文件->" + fileSub)
-        exit()
+        insertListBoxMessage("解析将会停止")
+    insertListBoxMessage("请确保需要解析的广告数据表在第一个...")
+    insertListBoxMessage("开始生成 ...")
     logging.warning("请确保需要解析的广告数据表在第一个...")
     logging.info("开始生成 ...")
 
-    main()
+    main(excelPath)
 
     jsonResult = json.dumps(mAdResultMap, indent=4, ensure_ascii=False)
     fileName = os.path.splitext(os.path.basename(excelPath))[0]
     fileDir = os.path.dirname(excelPath)
     resultAdFileJson = os.path.join(fileDir, fileName + "_xpad_ad_release_2.0.json")
+    insertListBoxMessage(resultAdFileJson)
     logging.info(resultAdFileJson)
 
     if os.path.exists(resultAdFileJson):
@@ -347,4 +377,55 @@ if __name__ == '__main__':
         resultAdFile.write(jsonResult)
         resultAdFile.close()
 
+    insertListBoxMessage("生成成功")
     logging.info("生成成功")
+
+
+def selectPath():
+    global filename
+    global path
+    filename = filedialog.askopenfilename(filetypes=[("excel格式", "xlsx")])
+    path.set(filename)
+
+
+def startCreateJson():
+    global listBox
+    listBox.delete(0, END)
+    global filename
+    startAndBuild(filename)
+    pass
+
+
+def creatMainUi():
+    global path
+    global listBox
+
+    root = Tk()
+    root.title("XPAD 2.0 json脚本生成工具")
+    root.geometry("1000x618")
+    root.resizable(False, False)
+
+    path = StringVar()
+
+    topFrame = Frame(root)
+    topFrame.pack(side=TOP)
+
+    Label(topFrame, text="目标路径:").pack(side=LEFT, padx=5, pady=10)
+    Entry(topFrame, textvariable=path).pack(side=LEFT, padx=5, pady=10)
+    Button(topFrame, text="路径选择", command=selectPath).pack(side=LEFT, padx=5, pady=10)
+    Button(topFrame, text="开始生成", command=startCreateJson).pack(side=LEFT, padx=5, pady=10)
+
+    bottomFrame = Frame(root)
+    scrollbar = Scrollbar(bottomFrame)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    listBox = Listbox(root, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=listBox.yview)
+    listBox.pack(side=LEFT, fill=BOTH, expand=YES, pady=10)
+    bottomFrame.pack(side=TOP, fill=BOTH, expand=YES)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    # startAndBuild()
+    creatMainUi()
--- XpadJsonBuild_data_pip.py
+++ XpadJsonBuild_data_pip.py
@@ -1,5 +1,8 @@
 # coding=utf-8
 import openpyxl, os, json, sys, logging
+from tkinter import *
+from tkinter import filedialog
+import datetime
 
 AD_TYPE_OPEN = "开屏"
 AD_TYPE_NATIVE_PLATE = "原生模版"
@@ -54,6 +57,18 @@ excelPath = ""
 
 mAdResultMap = {}
 
+filename = None
+path = None
+listBox: Listbox = None
+native_ad_refr_it: IntVar = None
+native_ad_refr_t_u: IntVar = None
+
+
+def insertListBoxMessage(item):
+    global listBox
+    listBox.insert("end", item)
+    listBox.see(END)
+
 
 def findTitleInColum(name, adSheet):
     maxColumn = int(adSheet.max_column)
@@ -61,7 +76,7 @@ def findTitleInColum(name, adSheet):
         titleValue = adSheet.cell(row=1, column=columnIndex + 1).value
         if titleValue == name:
             return columnIndex + 1
-
+    insertListBoxMessage("title 不存在 -" + name)
     logging.error("title 不存在 -" + name)
     exit()
 
@@ -72,6 +87,7 @@ def getTitleColumValue(name, adSheet):
     if value is not None:
         return value
 
+    insertListBoxMessage("当前 " + name + "值为 None")
     logging.warning("当前 " + name + "值为 None")
 
     return ""
@@ -97,6 +113,7 @@ def hasAdTypeString(adDetailsValuelist: list, typeName):
 def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
     adDetailsValuelist: list = adDetailsValue.split("-")
     if len(adDetailsValuelist) < 2:
+        insertListBoxMessage("备注 命名错误" + str(adSourceIdValue))
         logging.error("备注 命名错误" + str(adSourceIdValue))
 
     if platformValue == AD_TYPE_CSJ:
@@ -113,6 +130,7 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         elif hasAdTypeString(adDetailsValuelist, AD_TYPE_BANNER):
             return AD_TYPE_CSJ_PERSONAL_PLATE_BANNER
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "穿山甲的广告id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "穿山甲的广告id为 = " + str(adSourceIdValue))
 
     elif platformValue == AD_TYPE_YLH:
@@ -127,6 +145,7 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         if hasAdTypeString(adDetailsValuelist, AD_TYPE_BANNER):
             return AD_TYPE_YLH_BANNER
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "广点通id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "广点通id为 = " + str(adSourceIdValue))
 
         pass
@@ -141,19 +160,24 @@ def getAdExtraTypeValue(platformValue, adDetailsValue, adSourceIdValue):
         if hasAdTypeString(adDetailsValuelist, AD_TYPE_NATIVE):
             return AD_TYPE_KSH_PERSONAL_PLATE_FEED
         else:
+            insertListBoxMessage("不支持这种类型 " + adDetailsValue + "快手id为 = " + str(adSourceIdValue))
             logging.error("不支持这种类型 " + adDetailsValue + "快手id为 = " + str(adSourceIdValue))
 
         pass
     else:
+        insertListBoxMessage("没有这个类型 广告id是 = " + str(adSourceIdValue))
         logging.error("没有这个类型 广告id是 = " + str(adSourceIdValue))
         exit()
-
+    insertListBoxMessage("不支持这种类型 " + adDetailsValue + "id为 = " + str(adSourceIdValue))
     logging.error("不支持这种类型 " + adDetailsValue + "id为 = " + str(adSourceIdValue))
     return None
 
 
 def addChannelIds(slot: list, adSheet):
     maxColum = adSheet.max_row
+    global native_ad_refr_it
+    global native_ad_refr_t_u
+    global native_ad_new_usr_st
 
     for sidRowIndex in range(1, maxColum):
         currentIndex = sidRowIndex + 1
@@ -167,30 +191,34 @@ def addChannelIds(slot: list, adSheet):
                 sidDataPipItem["ad_sw_o"] = True
                 sidDataPipItem["ad_sw_n"] = True
                 sidDataPipItem["ad_pro_h"] = 0
-                sidDataPipItem["ad_new_usr_st"] = 253370736000000
+                sidDataPipItem["ad_new_usr_st"] = native_ad_new_usr_st.get()
                 sidDataPipItem["ad_refr_sw"] = False
                 sidDataPipItem["ad_refr_it"] = 10000
                 sidDataPipItem["ad_refr_t_u"] = 10
+                insertListBoxMessage("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
                 logging.info("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
             elif adUnitNameValue.find(AD_TYPE_INTERSTITIAL) > 0:
                 sidDataPipItem["ad_sw_o"] = True
                 sidDataPipItem["ad_sw_n"] = True
                 sidDataPipItem["ad_pro_h"] = 0
-                sidDataPipItem["ad_new_usr_st"] = 253370736000000
+                sidDataPipItem["ad_new_usr_st"] = native_ad_new_usr_st.get()
                 sidDataPipItem["ad_refr_sw"] = False
                 sidDataPipItem["ad_refr_it"] = 10000
                 sidDataPipItem["ad_refr_t_u"] = 10
+                insertListBoxMessage("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
                 logging.info("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
             elif adUnitNameValue.find(AD_TYPE_NATIVE) > 0:
                 sidDataPipItem["ad_sw_o"] = True
                 sidDataPipItem["ad_sw_n"] = True
                 sidDataPipItem["ad_pro_h"] = 0
-                sidDataPipItem["ad_new_usr_st"] = 253370736000000
+                sidDataPipItem["ad_new_usr_st"] = native_ad_new_usr_st.get()
                 sidDataPipItem["ad_refr_sw"] = True
-                sidDataPipItem["ad_refr_it"] = 10000
-                sidDataPipItem["ad_refr_t_u"] = 10
+                sidDataPipItem["ad_refr_it"] = native_ad_refr_it.get()
+                sidDataPipItem["ad_refr_t_u"] = native_ad_refr_t_u.get()
+                insertListBoxMessage("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
                 logging.info("" + adUnitNameValue + "----" + str(sidDataPipItem["ad_refr_sw"]))
             else:
+                insertListBoxMessage("这个类型不支持" + adUnitNameValue)
                 logging.warning("这个类型不支持" + adUnitNameValue)
 
         # platformValue = getCloumeValueColumValue(currentIndex, "Platform", adSheet)
@@ -204,13 +232,14 @@ def addChannelIds(slot: list, adSheet):
     pass
 
 
-def main():
+def main(excelPath):
     # 开始读取
     wb = openpyxl.load_workbook(excelPath)
     sheetNames = wb.sheetnames
     adSheet = wb[str(sheetNames[0])]
     maxColumn = int(adSheet.max_column)
-
+    insertListBoxMessage("最大列 = " + str(maxColumn))
+    insertListBoxMessage("最大行 = " + str(adSheet.max_row))
     print("最大列 = " + str(maxColumn))
     print("最大行 = " + str(adSheet.max_row))
 
@@ -225,27 +254,33 @@ def main():
     addChannelIds(slot, adSheet)
 
 
-if __name__ == '__main__':
+def startAndBuild(excelPath):
     logging.basicConfig(level=logging.INFO)
-    logging.info("XPAD 2.0 json脚本生成工具")
+    insertListBoxMessage("XPAD data pip json脚本生成工具")
+    logging.info("XPAD data pip json脚本生成工具")
     argLen = len(sys.argv)
-    if argLen < 2:
-        logging.error("请输入想要解析的文件")
-        exit()
-    excelPath = sys.argv[1]
+    # if argLen < 2:
+    #     logging.error("请输入想要解析的文件")
+    #     exit()
+    # excelPath = sys.argv[1]
 
     if not os.path.exists(excelPath):
+        insertListBoxMessage("需要解析的Excel文件 不存在")
+
         logging.error("需要解析的Excel文件 不存在")
         exit()
 
     fileSub = os.path.splitext(os.path.basename(excelPath))[1]
     if fileSub != ".xlsx":
+        insertListBoxMessage("请输入正确的Excel文件->" + fileSub)
         logging.error("请输入正确的Excel文件->" + fileSub)
         exit()
+    insertListBoxMessage("请确保需要解析的广告数据表在第一个...")
     logging.warning("请确保需要解析的广告数据表在第一个...")
+    insertListBoxMessage("开始生成 ...")
     logging.info("开始生成 ...")
 
-    main()
+    main(excelPath)
 
     jsonResult = json.dumps(mAdResultMap, indent=4, ensure_ascii=False)
     fileName = os.path.splitext(os.path.basename(excelPath))[0]
@@ -261,4 +296,83 @@ if __name__ == '__main__':
         resultAdFile = open(resultAdFileJson, 'a')
         resultAdFile.write(jsonResult)
         resultAdFile.close()
+    insertListBoxMessage("生成成功")
     logging.info("生成成功")
+
+
+def selectPath():
+    global filename
+    global path
+    filename = filedialog.askopenfilename(filetypes=[("excel格式", "xlsx")])
+    path.set(filename)
+
+
+def startCreateJson():
+    global listBox
+    listBox.delete(0, END)
+    global filename
+    startAndBuild(filename)
+    pass
+
+
+def creatMainUi():
+    global path
+    global listBox
+    global native_ad_refr_it
+    global native_ad_refr_t_u
+    global native_ad_new_usr_st
+
+    root = Tk()
+    root.title("XPAD data pip json脚本生成工具")
+    root.geometry("1000x618")
+    root.resizable(False, False)
+
+    path = StringVar()
+    native_ad_new_usr_st = IntVar()
+    native_ad_new_usr_st.set(253370736000000)
+
+    native_ad_refr_it = IntVar()
+    native_ad_refr_it.set(10000)
+    native_ad_refr_t_u = IntVar()
+    native_ad_refr_t_u.set(10)
+
+    topFrame = Frame(root)
+    topFrame.pack(side=TOP)
+
+    Label(topFrame, text="目标路径:").pack(side=LEFT, padx=5, pady=10)
+    Entry(topFrame, textvariable=path).pack(side=LEFT, padx=5, pady=10)
+    Button(topFrame, text="路径选择", command=selectPath).pack(side=LEFT, padx=5, pady=10)
+    Button(topFrame, text="开始生成", command=startCreateJson).pack(side=LEFT, padx=5, pady=10)
+
+    middleFrame = Frame(root)
+    middleFrame.pack(side=TOP)
+
+    Label(middleFrame, text="新用户时间戳").pack(side=TOP, padx=5, pady=10)
+    newUserAdFrame = Frame(middleFrame)
+    newUserAdFrame.pack(side=TOP)
+    Label(newUserAdFrame, text="ad_new_usr_st").pack(side=LEFT, padx=5, pady=10)
+    Entry(newUserAdFrame, textvariable=native_ad_new_usr_st).pack(side=LEFT, padx=5, pady=10)
+
+    Label(middleFrame, text="原生广告配置").pack(side=TOP, padx=5, pady=10)
+    nativeAdFrame = Frame(middleFrame)
+    nativeAdFrame.pack(side=TOP)
+
+    Label(nativeAdFrame, text="ad_refr_it").pack(side=LEFT, padx=5, pady=10)
+    Entry(nativeAdFrame, textvariable=native_ad_refr_it).pack(side=LEFT, padx=5, pady=10)
+    Label(nativeAdFrame, text="ad_refr_t_u").pack(side=LEFT, padx=5, pady=10)
+    Entry(nativeAdFrame, textvariable=native_ad_refr_t_u).pack(side=LEFT, padx=5, pady=10)
+
+    bottomFrame = Frame(root)
+    scrollbar = Scrollbar(bottomFrame)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    listBox = Listbox(root, yscrollcommand=scrollbar.set)
+    scrollbar.config(command=listBox.yview)
+    listBox.pack(side=LEFT, fill=BOTH, expand=YES, pady=10)
+    bottomFrame.pack(side=TOP, fill=BOTH, expand=YES)
+
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    # startAndBuild()
+    creatMainUi()
